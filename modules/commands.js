509692459db843ae3067831481cbf997d478f96a
Michael George <mgeorge9492@gmail.com>
2019-01-01 18:32:40 -0800

    added commands

--- modules/commands.js
+++ modules/commands.js
@@ -17,4 +17,17 @@ commands['@nick'] =  (data, userId, socketPool) => {
   socketPool[userId].nickname = data.target;
 };
 
+commands['@quit'] = (data, userId, socketPool) => {
+  events.emit('quit', data, userId, socketPool);
+};
+  
+commands['@list'] = (data, userId, socketPool, socketArray) => {
+  let userList = socketArray;
+  console.log(userList);
+};
+
+commands['@dm'] = (data, userId, socketPool, socketArray) => {
+  events.emit('dm', data, userId, socketPool, socketArray);
+};
+
 module.exports = {commands};
